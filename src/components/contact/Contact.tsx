f21e25a5fa4d5e200aed5defc0ef69e7b7b7e3ed
radubortan <bortanradu@gmail.com>
2023-11-03 15:58:21 +0100

    email sender

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,14 +1,39 @@
 import classes from "./contact.module.scss";
 import { motion, AnimatePresence } from "framer-motion";
 import * as variants from "./contact.variants.ts";
-import { useState } from "react";
+import { useState, useRef, FormEvent } from "react";
 import Checkmark from "./checkmark/Checkmark.tsx";
 import Loader from "./loader/Loader.tsx";
 import PhoneSvg from "./phoneSvg/PhoneSvg.tsx";
+import emailjs from "@emailjs/browser";
+import Cross from "./cross/Cross.tsx";
 
 const Contact = () => {
+  const formRef = useRef<HTMLFormElement>(null);
+
   const [isSent, setIsSent] = useState(false);
   const [isSending, setIsSending] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const isInitial = !isSending && !isSent && !isError;
+
+  const sendEmail = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setIsSending(true);
+    try {
+      await emailjs.sendForm(
+        "service_5zlqqxm",
+        "template_nhpgnvq",
+        formRef.current!,
+        "1JBZTvX7jz72hdRc4"
+      );
+      setIsSending(false);
+      setIsSent(true);
+    } catch (e) {
+      setIsSending(false);
+      setIsError(true);
+    }
+  };
 
   return (
     <motion.div
@@ -29,18 +54,6 @@ const Contact = () => {
           variants={variants.contactVariants}
         >
           <h2>Mail</h2>
-          <button
-            onClick={() => {
-              setIsSending(true);
-
-              setTimeout(() => {
-                setIsSending(false);
-                setIsSent(true);
-              }, 3000);
-            }}
-          >
-            click
-          </button>
           <p>bortanradu@gmail.com</p>
         </motion.div>
         <motion.div
@@ -64,13 +77,15 @@ const Contact = () => {
         <motion.form
           initial={{ opacity: 0 }}
           whileInView={{ opacity: 1, transition: { delay: 4, duration: 1 } }}
+          ref={formRef}
+          onSubmit={sendEmail}
         >
-          <input type="text" placeholder="Name" required />
-          <input type="email" placeholder="Email" required />
+          <input type="text" placeholder="Name" required name="name" />
+          <input type="email" placeholder="Email" required name="email" />
           <textarea name="message" placeholder="Message" rows={8} />
           <button>
             <AnimatePresence mode="wait">
-              {!isSent && !isSending && (
+              {isInitial && (
                 <motion.p
                   animate={isSent && { opacity: 0 }}
                   exit={{ opacity: 0, transition: { duration: 0.3 } }}
@@ -78,10 +93,11 @@ const Contact = () => {
                   Send
                 </motion.p>
               )}
-              {!isSent && isSending && <Loader />}
+              {isSending && <Loader />}
             </AnimatePresence>
 
-            {isSent && !isSending && <Checkmark />}
+            {isSent && <Checkmark />}
+            {isError && <Cross />}
           </button>
         </motion.form>
       </div>
