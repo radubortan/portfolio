77df83a6831dfed7605299c71d04d0edf98f1d9c
radubortan <bortanradu@gmail.com>
2023-11-04 12:12:19 +0100

    prevent sidebar popin on page load

--- src/components/sidebar/sidebar.variants.ts
+++ src/components/sidebar/sidebar.variants.ts
@@ -5,13 +5,23 @@ export const sidebarVariants = {
       type: "tween",
       duration: 1,
       delay: 0.1,
+
+      // needed so the sidebar doesn't pop in when the page loads
+      opacity: { duration: 0.01 },
     },
+    // needed so the sidebar doesn't pop in when the page loads
+    opacity: 1,
   },
   closed: {
     clipPath: "circle(0px at 50px 50px)",
     transition: {
       type: "tween",
       duration: 0.5,
+
+      // needed so the sidebar doesn't pop in when the page loads
+      opacity: { delay: 5, duration: 0.5 },
     },
+    // needed so the sidebar doesn't pop in when the page loads
+    opacity: 0,
   },
 };
