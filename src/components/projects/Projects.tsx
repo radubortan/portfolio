ef49d7ca928d31e31eec779131d6da848e5f8b80
radubortan <bortanradu@gmail.com>
2023-11-07 20:09:50 +0100

    use constants

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -16,58 +16,60 @@ const Projects = () => {
   const isMobileDevice = screenSize.width <= 768;
 
   return (
-    <motion.div
-      initial="initial"
-      whileInView="whileInView"
-      viewport={{
-        once: isMobileDevice,
-      }}
-      className={classes.container}
-    >
-      <AnimatePresence mode="wait">
-        {isModalOpen && (
-          <ProjectModal
-            project={selectedProject}
-            handleClose={() => {
-              setIsModalOpen(false);
-              document.body.style.overflow = "auto";
-            }}
-          />
-        )}
-      </AnimatePresence>
-
-      <div className={classes.titleContainer}>
-        <motion.h1 variants={variants.textVariants}>
-          Mes <span>Projets</span>
-        </motion.h1>
-      </div>
-
+    <div className={classes.container}>
       <motion.div
-        className={classes.listContainer}
-        variants={variants.textVariants}
+        className={classes.content}
+        initial="initial"
+        whileInView="whileInView"
+        viewport={{
+          once: isMobileDevice,
+        }}
       >
-        {projects.map((project) => {
-          return (
-            <motion.div
-              key={project.title}
-              className={classes.box}
-              whileHover={{
-                backgroundColor: "#d3d3d3",
-                color: "#000000",
+        <AnimatePresence mode="wait">
+          {isModalOpen && (
+            <ProjectModal
+              project={selectedProject}
+              handleClose={() => {
+                setIsModalOpen(false);
+                document.body.style.overflow = "auto";
               }}
-              onClick={() => {
-                document.body.style.overflow = "hidden";
-                setIsModalOpen(true);
-                setSelectedProject(project);
-              }}
-            >
-              <h2>{project.title}</h2>
-              <p className={classes.learnMore}>Plus d'infos</p>
-            </motion.div>
-          );
-        })}
+            />
+          )}
+        </AnimatePresence>
+
+        <div className={classes.titleContainer}>
+          <motion.h1 variants={variants.textVariants}>
+            Mes <span>Projets</span>
+          </motion.h1>
+        </div>
+
+        <motion.div
+          className={classes.listContainer}
+          variants={variants.textVariants}
+        >
+          {projects.map((project) => {
+            return (
+              <motion.div
+                key={project.title}
+                className={classes.box}
+                whileHover={{
+                  backgroundColor: "#d3d3d3",
+                  color: "#000000",
+                }}
+                onClick={() => {
+                  document.body.style.overflow = "hidden";
+                  setIsModalOpen(true);
+                  setSelectedProject(project);
+                }}
+              >
+                <h2>{project.title}</h2>
+                <p className={classes.learnMore}>Plus d'infos</p>
+              </motion.div>
+            );
+          })}
+        </motion.div>
       </motion.div>
-    </motion.div>
+    </div>
   );
 };
 
