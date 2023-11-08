f511e8d7758eb1080a9aa8144a297190e0ec03b0
radubortan <bortanradu@gmail.com>
2023-11-08 15:52:29 +0100

    refresh on breakpoint change

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,60 @@ import AboutMe from "./components/aboutMe/AboutMe";
 import Studies from "./components/studies/Studies";
 import Skills from "./components/skills/Skills";
 import Home from "./components/home/Home";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [initialWidth] = useState<number>(window.innerWidth);
+  let timer: number;
+
+  // refreshes the page when changing breakpoint to avoid broken animations
+  const handleResize = () => {
+    clearTimeout(timer);
+
+    const MOBILE = 768;
+    const TABLET = 1024;
+
+    let initialBreakpoint = "";
+    let currentBreakpoint = "";
+
+    if (initialWidth <= MOBILE) {
+      initialBreakpoint = "mobile";
+    } else if (initialWidth > MOBILE && initialWidth <= TABLET) {
+      initialBreakpoint = "tablet";
+    } else {
+      initialBreakpoint = "desktop";
+    }
+
+    const breakpoints = ["mobile", "tablet", "desktop"];
+    const nonInitialBreakpoints = breakpoints.filter(
+      (bp) => bp !== initialBreakpoint
+    );
+
+    const currentWidth = window.innerWidth;
+
+    if (currentWidth <= MOBILE) {
+      currentBreakpoint = "mobile";
+    } else if (currentWidth > MOBILE && currentWidth <= TABLET) {
+      currentBreakpoint = "tablet";
+    } else {
+      currentBreakpoint = "desktop";
+    }
+
+    if (nonInitialBreakpoints.includes(currentBreakpoint)) {
+      timer = setTimeout(() => {
+        window.location.reload();
+      }, 1000);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div>
       <Home />
