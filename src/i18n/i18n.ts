b7bdfbdcb3bef16b80567b58b04461f95346372e
radubortan <bortanradu@gmail.com>
2023-11-09 18:07:39 +0100

    setup i18next

---
+++ src/i18n/i18n.ts
@@ -0,0 +1,27 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+import englishTranslations from "./translations/en.json";
+import frenchTranslations from "./translations/fr.json";
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: "en",
+    debug: true,
+    interpolation: {
+      escapeValue: false,
+    },
+    resources: {
+      en: {
+        translation: englishTranslations,
+      },
+      fr: {
+        translation: frenchTranslations,
+      },
+    },
+  });
+
+export default i18n;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import i18n from "./i18n/i18n.ts";
+import { I18nextProvider } from "react-i18next";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
+  </React.StrictMode>
+);
